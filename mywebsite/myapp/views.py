5e0b102041561629d688441ee8004a1d70bd24df
nanamek <45305292+nanamek@users.noreply.github.com>
2023-04-22 14:57:34 +0700

    Edit HttpResponse to Render and Add Dirbase in setting

--- mywebsite/myapp/views.py
+++ mywebsite/myapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def Homepage(request):
-    return HttpResponse('<h1>Hello Python</h1>')
+    return render(request, 'myapp/homepage.html')
